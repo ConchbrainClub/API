fb46d42eefaeff497302dc60f5ee7fdf498cd583
lixinyang123 <lixinyangemil@outlook.com>
2021-07-25 00:45:47 +0800

    add database.sql & complete restore.ps1

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE Table Recommends (
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+    'title' VARCHAR(10),
+    'link' VARCHAR(10),
+    'desc' VARCHAR(20),
+    'lang' CHAR(10)
+);
--- restore.ps1
+++ restore.ps1
@@ -1,2 +1,11 @@
-Install-Module -Name Polaris -Scope CurrentUser -Force
-Install-Module -Name PSSQLite -Scope CurrentUser -Force
+if ($false -eq $(Test-Path Conchbrain.db)) {
+    sqlite Conchbrain.db ".read ./database.sql"
+}
+
+if ($null -eq $(Get-Module -ListAvailable Polaris)) {
+    Install-Module -Name Polaris -Scope CurrentUser -Force
+}
+
+if ($null -eq $(Get-Module -ListAvailable PSSQLite)) {
+    Install-Module -Name PSSQLite -Scope CurrentUser -Force
+}
