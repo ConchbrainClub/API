9da619e85ab1676cf7ce753d62e4899916d26a09
lixinyang123 <lixinyangemil@outlook.com>
2021-07-25 17:59:08 +0800

    complete database operation

--- database.sql
+++ database.sql
@@ -1,7 +1,8 @@
 CREATE Table Recommends (
-    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-    'title' VARCHAR(10),
-    'link' VARCHAR(10),
-    'desc' VARCHAR(20),
-    'lang' CHAR(10)
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "title" VARCHAR(10),
+    "cover" VARCHAR(10),
+    "desc" VARCHAR(20),
+    "link" VARCHAR(10),
+    "lang" CHAR(10)
 );
